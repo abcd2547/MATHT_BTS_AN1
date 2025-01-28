fe176e13d15ff1ea22ff29af1ab0e45a081cdd34
RIOU <D.RIOU@sio.local>
2025-01-28 13:14:54 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,36 @@
+# comparer 2 chaine de caractere
+#chaine 1: chaine de reference
+#chaine 2: chaine a comparer
+# renvoie True si chaine 1=chaine 2, false sinon
+def comparer2chaine (chaine1, chaine2):
+        if(chaine1==chaine2):
+            return True
+        else :
+            return False
+#verifier 4 caractere
+#chaine1:chaine a verifier
+#return:true si 4 caractere
+def verifier4caractere(chaine1):
+        if(len(chaine1)==4):
+            return True
+        else:
+            return False
+
+def liste_chiffre(chiffre):
+        chiffre = [0,1,2,3,4,5,6,7,8,9]
+
+def verifieunchiffre(chaine1):
+        if (int(chaine1)):
+            return True
+        else:
+            return False
+def code_correct(chaine2):
+      code_correct==1234
+      if(chaine2==1234):
+            return True
+      else:
+            return False
+    
+        
+
+
