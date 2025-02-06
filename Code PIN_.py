9f2f28ac56f2013f1085d7b9ef6419f57014b60c
dylan.r <71138817+abcd2547@users.noreply.github.com>
2025-02-06 22:37:47 +0100

    Add files via upload

--- Code PIN_.py
+++ Code PIN_.py
@@ -1,15 +1,16 @@
 
 #chaine 1: chaine de reference
 #chaine 2: chaine a comparer
+#les 2 chaine doivent etre en str sinon code marche pas et si on met (0000 en int, on ressort avec 1 seul 0)
+#chaine1 en str et chaine2 en int=code ne marche pas
 
-
-#'' met les chiffre en str au lien de int par defaut
+#'' met les chiffre en str au lieu de int par defaut
 Chaine2='6931'
 
 def saisie():
     #saisie du code utilisateur
     Chaine1 = input("renseignez le code PIN :")
-    #verifier si le code contient 4 caractère
+    #verifier la longueur de la saisie
     verifier4caractère(Chaine1)
 
 # comparer 2 chaine de caractere
@@ -27,7 +28,7 @@ def verifier4caractère(Chaine1):
     else:
         print("4 caractère SVP")
         saisie()
-
+#verifier si c'est des caractere numerique et non lettres
 def caractèrenumerique(Chaine1):
     if(Chaine1. isnumeric()):
          comparer2chaine(Chaine1,Chaine2)
