7dfd5feb59b99f810000e44e73ededf6c0789f6c
dylan.r <71138817+abcd2547@users.noreply.github.com>
2025-04-11 11:40:50 +0200

    Add files via upload

---
+++ Collection.py
@@ -0,0 +1,17 @@
+from random import randint
+
+#parcourir le tableau de la collection
+def collectioncomplete(collection):
+    res = True
+    for i in range(0, 9):
+        if collection[i]==0:
+            res = False
+    return res
+
+def ajoute(collection):
+    nb_aleatoire = randint(0, 9)
+    collection.append(nb_aleatoire)     #ajoute l'entier a la fin de la collection
+    return collection
+
+def test():
+    collection = []
