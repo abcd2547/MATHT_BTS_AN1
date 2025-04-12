b233353819b064ea142b4183835d44f9549e37ec
dylan.r <71138817+abcd2547@users.noreply.github.com>
2025-04-12 14:25:55 +0200

    Add files via upload

--- Collection.py
+++ Collection.py
@@ -13,3 +13,19 @@ def ajoute(collection):
     collection.append(nb_aleatoire)     #ajoute l'entier a la fin de la collection
     return collection
 
+def collectionestcomplete(collection):
+    for figurine in range(10):
+        if collection.count(figurine) < 2:
+            return False
+        return True
+
+def test():
+    collection = []
+    nb_achat = 0
+    while not collectionestcomplete(collection):
+        collection = ajoute(collection)
+        nb_achats = nb_achats + 1
+        print(f"Achat {nb_achat} : {collection}")
+    return nb_achat, collection
+
+
